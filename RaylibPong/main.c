69aaf49229b0f036c582824ad9da028bb5f48207
Nicolas Gallo <nicolaslucasgallo@gmail.com>
2023-04-18 15:45:05 -0500

    Can now move player

--- RaylibPong/main.c
+++ RaylibPong/main.c
@@ -1,26 +1,43 @@
 #include "raylib.h"
+
 struct Player{
-    float xPos;
-    float yPos;
-}
+    float posX;
+    float posY;
+    float width;
+    float height;
+    Color color;
+};
+
 int main(void)
 {
-   
     const int screenWidth = 800;
     const int screenHeight = 600;
-
+    
+    struct Player player1 = {10,screenHeight/2 , 30, 100, RED}; 
+    
     InitWindow(screenWidth, screenHeight, "raylib pong clone");
 
     SetTargetFPS(60);              
   
     while (!WindowShouldClose())   
     {
+        if(IsKeyDown(KEY_DOWN))
+        {
+            player1.posY += 10;
+        }  
         
+        if(IsKeyDown(KEY_UP))
+        {
+            player1.posY -= 10;
+        }
+
         BeginDrawing();
 
             ClearBackground(BLACK);
+            
+            DrawRectangle(player1.posX ,player1.posY, player1.width, player1.height, player1.color); 
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Player 1 can now move up and down", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
        
