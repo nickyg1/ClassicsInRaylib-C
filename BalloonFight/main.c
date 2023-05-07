73a1105f9a91e003e004f9c72b8fb503dc9818e3
Nicolas Gallo <nicolaslucasgallo@gmail.com>
2023-05-06 19:32:10 -0500

    working on game transitions

--- BalloonFight/main.c
+++ BalloonFight/main.c
@@ -1,10 +1,12 @@
 #include "raylib.h"
 
 void StartScreen();
+void GameScreen();
+void GameOverScreen();
 
 void InitGame();
 
-typedef enum GameState {STARTSCREEN, GAME, GAMEOVER} GameState;
+typedef enum GameState {STARTSCREEN = 0, GAME, GAMEOVER} GameState;
 
 struct Actor 
 {
@@ -15,15 +17,11 @@ struct Actor
     Color color; 
 
 };
-GameState gameState = STARTSCREEN;  
-int main(void)
+GameState gameState;  
+
+void GameChanger()
 {
-    
-    InitGame(); 
-    
-    
-    
-    switch (gameState)
+ switch (gameState)
     {
         case STARTSCREEN:
         { 
@@ -33,43 +31,55 @@ int main(void)
         
         case GAME:
         {
+            GameScreen();
 
         }break;
 
+        case GAMEOVER:
+        {
+            GameOverScreen();
+        } break;
     }
+
+}
+
+int main(void)
+{
+    
+    InitGame(); 
+
+    GameChanger();
    
     CloseWindow();        
    
     return 0;
 }
+
 const int screenWidth = 640;
 const int screenHeight = 480;
+
 void InitGame()
 {
-    
-
+    gameState = STARTSCREEN; 
     InitWindow(screenWidth, screenHeight, "Balloon Fight Clone");
 
     SetTargetFPS(60);    
     
 }
-
 void StartScreen()
 {
-    struct Actor startGame = {screenWidth * 0.5f , 100 , 100, 100, GREEN};
-    struct Actor secondPlayerGame = {screenWidth * 0.5f - 100, 160 , 100, 100, GREEN};
-    struct Actor exitGame = {screenWidth * 0.5f , 220 ,  100, 100, GREEN };
-    
-    struct Actor actors[] = {startGame, exitGame, secondPlayerGame };
-   
+    struct Actor startGame = {screenWidth * 0.5f, 100, 100, 100, GREEN};
+    struct Actor secondPlayerGame = {screenWidth * 0.5f - 100, 160, 100, 100, GREEN};
+    struct Actor exitGame = {screenWidth * 0.5f, 220, 100, 100, GREEN};
+
+    struct Actor actors[] = {startGame, exitGame, secondPlayerGame};
+
     //datatype size_variable = *(&array_name + 1) - array_name;
     int counterMax = *(&actors + 1) - actors;//pointer arithmetic 
 
-    int counter = 0; 
-    
-    int exitBool;
+    int counter = 0;
 
-    while (!WindowShouldClose() && exitBool )    
+    while (!WindowShouldClose() && gameState == STARTSCREEN)    
     {
 
         if(IsKeyPressed(KEY_UP))
@@ -105,6 +115,7 @@ void StartScreen()
                if(IsKeyPressed(KEY_ENTER))
                {
                     gameState = GAME;
+                  GameChanger();
                }
 
             }break;
@@ -118,8 +129,12 @@ void StartScreen()
                
                 if(IsKeyPressed(KEY_ENTER))
                 {
+                    
                    gameState = GAME;
+                   GameChanger();
+                
                 }
+
             }break;
 
             case 2:
@@ -131,23 +146,41 @@ void StartScreen()
                
                 if(IsKeyPressed(KEY_ENTER))
                 {
-                  exitBool = 0; 
+                  gameState = GAMEOVER;
+                  GameChanger();
                 }
 
             }break;
-        }
+       }
 
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
         DrawRectangle(startGame.x,  startGame.y,  startGame.width, startGame.height, startGame.color); 
         DrawRectangle(secondPlayerGame.x,  secondPlayerGame.y,  secondPlayerGame.width,secondPlayerGame.height, secondPlayerGame.color); 
         DrawRectangle(exitGame.x,  exitGame.y,  exitGame.width, exitGame.height, exitGame.color); 
-
         EndDrawing();
-       
     }
+}
 
+void GameScreen()
+{
+    struct Actor player = {10,10,10,10,BLUE};
+
+    while (!WindowShouldClose() && gameState == GAME)    
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawRectangle(player.x, player.y, player.width, player.height, player.color);
+        
+        EndDrawing();
+   }
+}
+
+void GameOverScreen()
+{
+     EndDrawing();
 }
 
